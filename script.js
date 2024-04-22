e79b7d364a23cd13a570d8ac7f029acfd34518c0
lauracakste <112856606+lauracakste@users.noreply.github.com>
2024-04-22 16:04:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //console.log(1)
-    let zina = document.querySelector('zina').value;
-    let zinas = document.querySelector('cataZina');
+    let zina = document.querySelector('.zina');
+    let zinas = document.querySelector('.cataZina');
 
 function sutitZinu()
 {
